a866c28905e142c36a96a832ba1433678f5bbe21
EMYLY09BATISTASILVA <emilybsilva09@gmail.com>
2023-10-24 19:38:38 -0300

    nvdrfr

--- js/script.js
+++ js/script.js
@@ -1,50 +1,56 @@
-const pokedex = document.getElementById("pokedex");
-const apiUrl = "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20";
-const inputFiltrar = document.getElementById("txtFiltrar");
+document.addEventListener("DOMContentLoaded", function() {
+    const pokedex = document.getElementById("pokedex");
+    const searchButton = document.getElementById("searchButton");
+    const searchInput = document.getElementById("searchInput");
+    const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+    let pokemonData = [];
 
-
-
-async function Carregarpokemons(){
-    const urlEndPoint = "https://pokeapi.co/api/v2/pokemon/?offset=40&limit=20";
-    const options = {
-        method: 'GET'
-    }
-
-
-    try {
-        //código válido
-       const resposta = await fetch(urlEndPoint, options);
-       const dados = await resposta.json();
-
-       console.log(dados);
-
-       dados.forEach((item) => {
-       
-        li.textContent = item.song + "-" + item.artist + "-" + item.genre;
-       
-       });
-
-    } catch (error) {
-        console.log('Error:' + error);
-        
-    }
-
-}
-Carregarpokemons();
-
-inputFiltrar.addEventListener("input", () => {
-    let filtro = inputFiltrar.value.toLowerCase();
-  
-
-    for(const musica of listagem){
-        const textoMusica = musica.textContent.toLowerCase();  
-
-        if(textoMusica.includes(filtro)){
-            musica.style.display = "flex";
-        }else{
-            musica.style.display = "none";
+    async function fetchPokemonData(url) {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data.results;
+        } catch (error) {
+            console.error("Erro ao buscar dados dos Pokémon: " + error);
         }
     }
-});
 
-displayPokemon();
+    async function displayPokemon(pokemonData) {
+        pokedex.innerHTML = "";
+
+        for (const pokemon of pokemonData) {
+            const response = await fetch(pokemon.url);
+            const data = await response.json();
+            const name = data.name;
+            const imageUrl = data.sprites.front_default;
+
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `
+                <img src="${imageUrl}" alt="${name}">
+                <h2>${name}</h2>
+            `;
+
+            pokedex.appendChild(card);
+        }
+    }
+
+    // Função para buscar Pokémon por nome
+    function searchPokemonByName() {
+        const searchTerm = searchInput.value.toLowerCase();
+        const filteredPokemon = pokemonData.filter(pokemon => pokemon.name.includes(searchTerm));
+        displayPokemon(filteredPokemon);
+    }
+
+    // Função para carregar todos os Pokémon iniciais
+    async function loadInitialPokemon() {
+        pokemonData = await fetchPokemonData(apiUrl);
+        displayPokemon(pokemonData);
+    }
+
+    // Carregar todos os Pokémon iniciais ao carregar a página
+    loadInitialPokemon();
+
+    // Adicionar evento de clique ao botão de pesquisa
+    searchButton.addEventListener("click", searchPokemonByName);
+});
